43032c96483083b7fd982ce1c87fd6f500ef2ff5
neo-photo <neo@emailfach.de>
2026-09-16 10:30:38 +0200

    splitted settings

---
+++ devproject/settings_local_org.py
@@ -0,0 +1,10 @@
+
+DATABASES["default"]["NAME"] = "misago392"
+DATABASES["default"]["USER"] = "spirito"
+DATABASES["default"]["PASSWORD"] = "spirito"
+    
+MEDIA_ROOT = os.path.join(BASE_DIR, "../../media")
+
+LOGGING["handlers"]["file"]["filename"] = "../general.log"
+LOGGING["loggers"]["django"]["LEVEL"] = "DEBUG"
+# LOGLEVEL: DEBUG INFO WARNING ERROR CRITICAL
---
+++ devproject/settings_secure.py
@@ -0,0 +1,19 @@
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        # Misago requires PostgreSQL to run
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "xxx",
+        "USER": "xxx",
+        "PASSWORD": "xxx",
+        "HOST": "127.0.0.1",
+        "PORT": 5432,
+    }
+}
+# If either of these settings is empty, Django won't attempt authentication.
+
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+
